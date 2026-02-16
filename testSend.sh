34e8400a16a4ea578f463d12e2bdc86f254df205
Miguel <jml@isep.ipp.pt>
2026-02-16 23:41:45 +0000

    update

--- testSend.sh
+++ testSend.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-./sendSMS -D -s 961234567 "À noite, é o sábio que vê maçãs e põe órgãos únicos."
-./sendSMS -D -s 961234567 "The quick brown fox jumps over the lazy dog."
-./sendSMS -D -s 961234567 "The quick brown fox and the smart white horse, went to the quiet blue lake and they jumped over the lazy yellow dog."
+destPhoneNum=961234567
+options="-D -s"
+./sendSMS $options $destPhoneNum "À noite, é o sábio que vê maçãs e põe órgãos únicos."
+./sendSMS $options $destPhoneNum "The quick brown fox jumps over the lazy dog."
+./sendSMS $options $destPhoneNum "The quick brown fox and the smart white horse, went to the quiet blue lake and they jumped over the lazy yellow dog."
 
 
 
